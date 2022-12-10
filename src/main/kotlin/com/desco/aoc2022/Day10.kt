29d41634ee4a86721b2713497aeb5f0a4659dcb5
Desco <63560727+Desco1@users.noreply.github.com>
2022-12-10 11:28:58 +0100

    pretty neat idea to improve readability of the output, yoinked straight from the subreddit

--- src/main/kotlin/com/desco/aoc2022/Day10.kt
+++ src/main/kotlin/com/desco/aoc2022/Day10.kt
@@ -3,6 +3,8 @@ package com.desco.aoc2022
 import com.desco.Day
 import kotlin.math.floor
 
+fun main() = Day10.second()
+
 object Day10: Day(10, 2022) {
 
     override fun partOne(): Any {
@@ -10,18 +12,18 @@ object Day10: Day(10, 2022) {
     }
 
     override fun partTwo(): Any {
-        val screen = Array(6) { Array(40) { " " } }
+        val screen = Array(6) { Array(40) { "░░" } }
 
         repeat(240) {
             val reg = findSignal(it + 1)
             if (reg in it % 40 - 1 .. it % 40 + 1) {
-                screen[floor(it / 40.0).toInt()][it % 40] = "#"
+                screen[floor(it / 40.0).toInt()][it % 40] = "██"
             }
         }
 
         // This is just so the letters are clearer to read
         // Also the initial \n to not have the first row be shifted right
-        return "\n${screen.joinToString("\n") { it.joinToString(" ") }}"
+        return "\n${screen.joinToString("\n") { it.joinToString("") }}"
     }
 
     private fun findSignal(cycle: Int): Int {
